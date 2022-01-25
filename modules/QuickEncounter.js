89249fc2c316a51a2e4d19d5b8224832cf23f016
opus1217 <jdpugh2@gmail.com>
2022-01-25 12:58:11 -0800

    v1.0.1 25-Jan-2022 Versioned to 1.0.1 and add summary of changes (adding Instant Encounters)

--- modules/QuickEncounter.js
+++ modules/QuickEncounter.js
@@ -193,7 +193,7 @@ import {QESheet} from './QESheet.js';
 
 export const QE = {
     MODULE_NAME : "quick-encounters",
-    MODULE_VERSION : "0.9.7",
+    MODULE_VERSION : "1.0.1",
     TOKENS_FLAG_KEY : "tokens",
     QE_JSON_FLAG_KEY : "quickEncounter"
 }
